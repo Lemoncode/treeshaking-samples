406bb3e8bd10630fab6356fc68eb7376bed182f9
Braulio <braulio.diez@gmail.com>
2018-05-09 17:53:06 +0200

    starting point

---
+++ typescript/03A barrels/src/app.tsx
@@ -0,0 +1,41 @@
+import * as React from 'react';
+import { HelloComponent } from './hello';
+import { NameEditComponent } from './nameEdit';
+import {AppBar} from "material-ui";
+import {MuiThemeProvider} from "material-ui/styles";
+
+
+interface Props {
+}
+
+interface State {
+  userName: string;
+}
+
+export class App extends React.Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+
+    this.state = { userName: 'defaultUserName' };
+  }
+
+  setUsernameState = (event) => {
+    this.setState({ userName: event.target.value });
+  }
+
+
+  public render() {
+    return (
+      <>
+        <MuiThemeProvider>
+          <AppBar
+              title="Title"
+              iconClassNameRight="muidocs-icon-navigation-expand-more"
+            />
+          <HelloComponent userName={this.state.userName} />
+          <NameEditComponent userName={this.state.userName} onChange={this.setUsernameState} />
+        </MuiThemeProvider>
+      </>
+    );
+  }
+}
