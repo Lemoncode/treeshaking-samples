fa57c9568015e61a269a8d87b5cc2975f1768a04
Igor Alvarez <igor.alvarez@gmail.com>
2018-05-26 19:02:08 +0200

    Delete

--- ES6/02 treeshaking-material-ui/src/hello.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-
-export const HelloComponent = (props) =>
-    <h2>Hello user: {props.userName}!</h2>;
-
-HelloComponent.propTypes = {
-    userName: PropTypes.string.isRequired
-};
